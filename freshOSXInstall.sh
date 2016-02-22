f8f599bd971e43bd8fd1ace4d4d96de4bd836b3d
Dawson Botsford <DawsonBotsford@gmail.com>
2016-02-22 11:53:21 -0700

    add fresh osx install script

---
+++ freshOSXInstall.sh
@@ -0,0 +1,41 @@
+# Install XCode (or just XCode tools)
+# Install Chrome
+# Install Iterm2 https://www.iterm2.com/downloads.html
+
+#Does not require XCode tools
+#Set git configs
+git config --global user.name "Dawson Botsford"
+git config --global user.email "DawsonBotsford@gmail.com"
+# setup ssh keys on github
+
+#Requires XCode tools:
+cd ~
+#Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Dotfiles
+git clone git@github.com:dawsonbotsford/dotfiles.git
+cd dotfiles/scripts
+sh make.sh
+
+brew update
+brew install wget
+brew install node
+npm install -g grunt-cli
+npm install -g gh
+npm install -g npm-name-cli
+npm install -g hicat
+
+brew install heroku-toolbelt
+heroku update
+brew install vim
+brew install cmake
+
+
+# Neobundle install
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+
+sudo easy_install pip
+sudo pip install grip
+
