e2d6a6d3159ffd6aaa5cca2fe5ec6cfafbf7b4c8
Dawson Botsford <dawsonb@uber.com>
2016-06-28 14:49:35 -0700

    npm run watch command

--- zshrcs/npmAliases.sh
+++ zshrcs/npmAliases.sh
@@ -4,15 +4,16 @@ alias ni='npm install '
 alias nis='npm install --save '
 alias nig='npm install --global '
 alias nid='npm install --save-dev '
-alias nl='npm link'
+alias nl='npm link '
 alias l='link'
-alias nv='npm version'
+alias nv='npm version '
 alias nvp='npm version patch'
-alias np='npm publish'
-alias ns='npm start'
-alias pub='gpom && npm publish'
+alias np='npm publish '
+alias ns='npm start '
+alias pub='gpom && npm publish '
 
-alias npm-exec='PATH=$(npm bin):$PATH'
+alias npm-exec='PATH=$(npm bin):$PATH '
+alias nrw='npm run watch '
 
 why() {
   npm view "$1" description
