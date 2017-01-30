f16e0d182c796d7e286ad2e512d901809c510509
Dawson Botsford <dawsonb@uber.com>
2017-01-30 10:44:51 -0800

    fix link script

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,26 +3,17 @@
 # Creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
-dir=~/Dropbox/dotfiles/link               # dotfiles directory
-olddir=~/Dropbox/dotfiles_old             # old dotfiles backup directory
-# files="vimrc zshrc eslintrc gitignore"    # list of files/folders to symlink in homedir
+dir="$HOME/Dropbox/dotfiles/link"               # dotfiles directory
+olddir=/tmp/dotfiles_old             # old dotfiles backup directory
 files="vimrc zshrc vim"    # list of files/folders to symlink in homedir
 
-# create dotfiles_old in homedir
-printf "\nCreating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-printf "...done"
-
-# change to the dotfiles directory
-printf "\nChanging to the $dir directory"
-cd $dir
-printf "...done"
+cd "$dir" || exit
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    printf "\nMoving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    printf "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    # printf "\nMoving any existing dotfiles from ~ to $olddir"
+    mv "$HOME/.$file" "$olddir/$file"
+
+    ln -s "$dir/$file" "$HOME/.$file"
 done
