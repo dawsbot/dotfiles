3d69fda81ead7d20c64324fe273720945abdc8f9
Dawson Botsford <DawsonBotsford@gmail.com>
2017-04-22 20:37:04 -0600

    fix weird vim/neovim aliases

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,19 +3,18 @@
 # Creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-dir="$HOME/Dropbox/dotfiles/link" # dotfiles directory
-files="zshrc vim"    # list of files/folders to symlink in homedir
+DIR="$HOME/Dropbox/dotfiles/link" # dotfiles directory
+FILES="zshrc vim"    # list of files/folders to symlink in homedir
 
-cd "$dir" || exit
+cd "$DIR" || exit
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
+for file in $FILES; do
   rm -rf "$HOME/.$file"
-  ln -s "$HOME/$file" "$HOME/.$file"
+  ln -s "$DIR/$file" "$HOME/.$file"
 done
 
 # link neovimrc
 rm -rf ~/.config/nvim
 mkdir ~/.config/nvim
-ln -s "$dir/vimrc" ~/.config/nvim/init.vim
-# ln -s ~/.vim ~/.config/nvim
+ln -s "$DIR/vimrc" ~/.config/nvim/init.vim
